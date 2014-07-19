784f07594261e64e834c401fd6095e4a3305493d
root <root@firasimus.(none)>
2014-07-19 02:43:05 -0400

    Most Recent

--- lempMR.sh
+++ lempMR.sh
@@ -28,40 +28,52 @@ echo "Deploy Success" | mail -s "Deploy Success" fzebib@gmail.com
 
 ### Update DBM
 function UPDATEDBM {
-echo "#################"
 echo "Adding repositories and Updating Yum"
-sleep 3s
+echo -ne '#####                     (33%)\r'
+sleep 1
 ssh root@$IP rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm > /dev/null 2>&1
 ssh root@$IP rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm > /dev/null 2>&1
+echo -ne '#############             (66%)\r'
+sleep 1
 ssh root@$IP  yum update -yq > /dev/null 2>&1
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
 }
 
 
 ### Install of LEMP + NFS + MAIL
 function LEMP {
-echo "##############"
 echo "Installing appropriate daemons"
+echo -ne '#####                     (33%)\r'
 ssh root@$IP yum install -y $NFSUTIL $MAIL $NGINX $MYSQL $LAMPPHP > /dev/null 2>&1
+echo -ne '#############             (66%)\r'
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
 }
 
 ###Mounting Remotely via Fstab
 function MOUNT {
-sleep 3s
-echo "################"
 echo "Mounting NFS"
+echo -ne '#####                     (33%)\r'
 ssh root@$IP "echo 162.243.67.60:/var/www/html /var/www/html  nfs      auto,noatime,nolock,bg,nfsvers=3,intr,tcp,actimeo=1800 0 0 >> /etc/fstab"
 sleep 3s
-if (ssh root@$IP '[ -d /directory ]')
+if (ssh root@$IP '[ -d /var/www/html ]')
+echo -ne '#############             (66%)\r'
 then
 sleep 3s
-echo "Directory existing, mounting now"
+echo "Directory exists, mounting now"
 ssh root@$IP mount -a
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
 else
-echo "Mount directory does not exist, creating"
+echo -n Mount directory does not exist, creating and mounting.
+sleep 1
 ssh root@$IP  'mkdir -p /var/www/html/'
-sleep 2s
-echo "Mounting now"
+echo -ne '#############             (66%)\r'
+sleep 1
 ssh root@$IP mount -a
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
 fi
 }
 
@@ -69,29 +81,23 @@ fi
 #### Autostart options for LEMP
 
 function AUTOSTART {
-echo "######################"
 echo "Mysql set for Autostart"
-echo "#####################"
+echo -ne '#####                     (33%)\r'
+sleep 1
 ssh root@$IP chkconfig mysqld on
-sleep 3s
 
-echo "####################"
+
 echo "Nginx set for Autostart"
-echo "####################"
 ssh root@$IP chkconfig nginx on
-sleep 3s
-
-echo "####################"
+echo -ne '#############             (66%)\r'
+sleep 1
 echo "Php-fpm set for Autostart"
-echo "####################"
 ssh root@$IP chkconfig php-fpm on
-sleep 3s
 
-echo "####################"
 echo "NFS set for Autostart"
-echo "######################"
 ssh root@$IP chkconfig nfs on && service rpcbind start && service nfs start
-sleep 3s
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
 }
 
 ##SSH-Key Access
@@ -109,6 +115,9 @@ done
 ###NFS Mount access
 #### This "nfs.txt" should include the nfs server you are editing. The "hostname.txt" should be the new server you are giving access to.
 function NFSMOUNT {
+echo "Adding server to NFS"
+echo -ne '#####                     (33%)\r'
+sleep 1
 for NFS in $(cat nfs.txt)
 do
 	for hostname in $(cat hostname.txt)
@@ -117,12 +126,12 @@ IP2=`ssh root@$hostname  ifconfig | grep Bcast | cut -d: -f2 | cut -d" " -f1`
 for IPREMOTE in $IP2
 do
 ssh root@$NFS 'echo -e /var/www/html "'$IPREMOTE'(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports'
+echo -ne '#############             (66%)\r'
+sleep 1
 ssh root@$NFS exportfs -a
-sleep 3s
-echo "###########"
 echo "NFS updated with additional servers"
-echo "###########"
-sleep 3s
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
 done
 done
 done
