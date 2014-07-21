7e4d95f0405d27cb112104c9e1ac40edd4944b6e
root <root@firasimus.(none)>
2014-07-20 22:56:02 -0400

    Upto Date Script

--- lempMR.sh
+++ lempMR.sh
@@ -14,6 +14,7 @@ LAMPPHP="php-fpm php-mysql"
 
 MAIL="mail"
 
+
 NFSUTIL="nfs-utils nfs-utils-lib"
 
 ###Define IPs
@@ -30,32 +31,23 @@ echo "Deploy Success" | mail -s "Deploy Success" fzebib@gmail.com
 ### Update DBM
 function UPDATEDBM {
 echo "Adding repositories and Updating Yum"
-echo -ne '#####                     (33%)\r'
 sleep 1
 ssh root@$IP rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm > /dev/null 2>&1
 ssh root@$IP rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm > /dev/null 2>&1
-echo -ne '#############             (66%)\r'
 sleep 1
 ssh root@$IP  yum update -yq > /dev/null 2>&1
-echo -ne '#######################   (100%)\r'
-echo -ne '\n'
 }
 
 
 ### Install of LEMP + NFS + MAIL
 function LEMP {
 echo "Installing appropriate daemons"
-echo -ne '#####                     (33%)\r'
-ssh root@$IP yum install -y $NFSUTIL $MAIL $NGINX $MYSQL $LAMPPHP > /dev/null 2>&1
-echo -ne '#############             (66%)\r'
-echo -ne '#######################   (100%)\r'
-echo -ne '\n'
+ssh root@$IP yum install -y $NFSUTIL $MAIL $NGINX $LAMPPHP > /dev/null 2>&1
 }
 
 ###Mounting Remotely via Fstab
 function MOUNT {
 echo "Mounting NFS"
-echo -ne '#####                     (33%)\r'
 if ssh root@$IP grep -Fq "162.243.67.60:/var/www/html" /etc/fstab
 then
 	echo "Settings already exist for Fstab"
@@ -64,46 +56,43 @@ ssh root@$IP "echo 162.243.67.60:/var/www/html /var/www/html  nfs      auto,noat
 fi
 sleep 3s
 if (ssh root@$IP '[ -d /var/www/html ]')
-echo -ne '#############             (66%)\r'
 then
 sleep 3s
 echo "Directory exists, mounting now"
 ssh root@$IP mount -a
-echo -ne '#######################   (100%)\r'
-echo -ne '\n'
 else
-echo -n Mount directory does not exist, creating and mounting.
+echo "Mount directory does not exist, creating and mounting"
 sleep 1
-ssh root@$IP  'mkdir -p /var/www/html/'
-echo -ne '#############             (66%)\r'
+ssh root@$IP mkdir -p '/var/www/html/'
 sleep 1
 ssh root@$IP mount -a
-echo -ne '#######################   (100%)\r'
-echo -ne '\n'
 fi
 }
 
 #### Autostart options for LEMP
 
 function AUTOSTART {
-echo "Mysql set for Autostart"
-echo -ne '#####                     (33%)\r'
-sleep 1
-ssh root@$IP chkconfig mysqld on
+#echo "Mysql set for Autostart"
+#sleep 1
+#ssh root@$IP service mysqld restart && chkconfig mysqld on
 
 
 echo "Nginx set for Autostart"
-ssh root@$IP chkconfig nginx on
-echo -ne '#############             (66%)\r'
+ssh root@$IP service nginx restart && chkconfig nginx on
 sleep 1
 echo "Php-fpm set for Autostart"
-ssh root@$IP chkconfig php-fpm on
+ssh root@$IP service php-fpm restart && chkconfig php-fpm on
+
+}
+
+
+##NFS Autostart
+function NFSAUTOSTART {
 
 echo "NFS set for Autostart"
-ssh root@$IP chkconfig nfs on && service rpcbind start && service nfs start
-echo -ne '#######################   (100%)\r'
-echo -ne '\n'
+ssh root@$IP service nfs start && chkconfig nfs on && service rpcbind start
 }
+
 
 ##SSH-Key Access
 for NFS in $(cat nfs.txt)
@@ -121,7 +110,6 @@ done
 #### This "nfs.txt" should include the nfs server you are editing. The "hostname.txt" should be the new server you are giving access to.
 function NFSMOUNT {
 echo "Adding server to NFS"
-echo -ne '#####                     (33%)\r'
 sleep 1
 for NFS in $(cat nfs.txt)
 do
@@ -135,18 +123,15 @@ then
 echo "'$IPREMOTE' already exists, moving onto next IP"
 else 
 ssh root@$NFS 'echo -e /var/www/html "'$IPREMOTE'(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports' || { echo 'Failed on NFS mount Access' ; exit 1; }
-
-echo -ne '#############             (66%)\r'
 sleep 1
 fi
 ssh root@$NFS exportfs -a  || { echo 'Failed on NFS mount export' ; exit 1; }
 echo "NFS updated with additional servers"
-echo -ne '#######################   (100%)\r'
-echo -ne '\n'
 done
 done
 done
 }
+
 
 ###Nginx Configuration
 function NGINXCONF {
@@ -163,6 +148,14 @@ scp default.conf root@$IP:/etc/nginx/conf.d/default.conf
 else
 echo "Conf file for $IP upto date"
 fi
+}
+
+##MYSQL remote access grants
+function MYSQL {
+for MYSQLPW in $(cat mysqlpw.txt)
+do
+ssh root@$NFS "mysql -u root -p$MYSQLPW --execute='grant all privileges on wordpress.* to \"firaswp\"@\"$IP\" identified by \"password123\"'"
+done
 }
 
 if UPDATEDBM ;
@@ -184,18 +177,22 @@ echo "Deploy fail at LEMP install" | mail -s "Deploy Fail" fzebib@gmail.com
 exit 1
 fi
 
+
+NFSAUTOSTART
+
+MYSQL
+NGINXCONFUP
+NFSMOUNT
+MOUNT
+
 if AUTOSTART ;
 then
 echo "..Success"
 else
 echo "Problem with setting daemons for autostart"
 echo "Deploy fail at Runlevel update" | mail -s "Deploy Fail" fzebib@gmail.com
-exit 1
+#exit 1
 fi
-
-NGINXCONFUP
-NFSMOUNT
-MOUNT
 
 
 SUCCESS
